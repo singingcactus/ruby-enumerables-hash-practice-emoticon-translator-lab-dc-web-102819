2693c7741f61ad588f96795494c00276e9976808
Matt Bechtel <bechma02@gmail.com>
2019-10-23 02:43:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -35,11 +35,11 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
     emoticon_library = load_library(file_path)
-    if emoticon_library[:get_j_emoticon][emoticon]
+#    if emoticon_library[:get_j_emoticon][emoticon]
       emoticon_library[:get_j_emoticon][emoticon]
-    else
-      return "Sorry, that emoticon was not found"
-    end
+#    else
+#      return "Sorry, that emoticon was not found"
+#    end
 end
 
 def get_english_meaning(file_path, emoticon)
